a987cb0add798b26ad50b1190519ff1cbf7a32f9
Anna Larionova <larionova.a2000@gmail.com>
2023-07-10 21:11:15 +0300

    hotfix: add submit button to another component

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -25,7 +25,7 @@ function AddPlacePopup({isOpen, onClose, onSubmit, buttonText}) {
   }
 
   return (
-    <PopupWithForm onSubmit={handleSubmit} isOpen={isOpen} onClose={onClose} title={"Новое место"} name={"cardInfo"} styleClass={"add"} buttonText={"Сохранить"}>
+    <PopupWithForm onSubmit={handleSubmit} isOpen={isOpen} onClose={onClose} title={"Новое место"} name={"cardInfo"} styleClass={"add"} buttonText={"Создать"}>
       <input value={placeName} onChange={handlePlaceNameChange} id="cardName-input" name="name" type="text" placeholder="Название" className="popup__text popup__text_type_name" required minLength="2" maxLength="30"/>
       <span className="cardName-input-error popup__text-error"></span>
       <input value={placeLink} onChange={handlePlaceLinkChange} id="photo-link-input" name="photo-link" type="url" placeholder="Ссылка на картинку" className="popup__text popup__text_type_photo-link" required/>
